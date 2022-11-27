2ffd2523d1a4ed0137c3c73069882d64626afa29
lintang-b-s <lintangbirdasaputra@gmail.com>
2022-11-27 19:07:11 +0700

    pass say hello world test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -3,32 +3,40 @@ const ClientError = require('../../Commons/exceptions/ClientError');
 const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
 const users = require('../../Interfaces/http/api/users');
 const authentications = require('../../Interfaces/http/api/authentications');
-
-const createServer = async (container) => {
+ 
+const createServer = async (injections) => {
   const server = Hapi.server({
     host: process.env.HOST,
     port: process.env.PORT,
   });
-
+ 
   await server.register([
     {
       plugin: users,
-      options: { container },
+      options: { injections },
     },
     {
       plugin: authentications,
-      options: { container },
+      options: { injections },
     },
   ]);
-
+ 
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      value: 'Hello world!',
+    }),
+  });
+ 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
-
+ 
     if (response instanceof Error) {
       // bila response tersebut error, tangani sesuai kebutuhan
       const translatedError = DomainErrorTranslator.translate(response);
-
+ 
       // penanganan client error secara internal.
       if (translatedError instanceof ClientError) {
         const newResponse = h.response({
@@ -38,12 +46,12 @@ const createServer = async (container) => {
         newResponse.code(translatedError.statusCode);
         return newResponse;
       }
-
+ 
       // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
       if (!translatedError.isServer) {
         return h.continue;
       }
-
+ 
       // penanganan server error sesuai kebutuhan
       const newResponse = h.response({
         status: 'error',
@@ -52,12 +60,12 @@ const createServer = async (container) => {
       newResponse.code(500);
       return newResponse;
     }
-
+ 
     // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
     return h.continue;
   });
-
+ 
   return server;
 };
-
+ 
 module.exports = createServer;
